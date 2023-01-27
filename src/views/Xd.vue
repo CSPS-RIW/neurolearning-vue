fd1f9291ff1ba95d04dd4f3c0431ec49c62b548a
noamstul <59657151+noamstul@users.noreply.github.com>
2023-01-27 10:32:43 -0500

    added go back to first page text

--- src/views/Xd.vue
+++ src/views/Xd.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="wrapper">
         <header>
-            <h1 class="text-center pt-3">Your Custom Toolkit</h1>
+            <h1 class="text-center">Your Custom Toolkit</h1>
         </header>
         <main>
             <div class="holster">
-                <div class="questionnaire-wrapper" aria-live="polite" role="region">
+                <div class="questionnaire-wrapper" aria-live="polite" role="region" aria-label="Questionnaire">
                     <div class="questionnaire-header">
                         <h2 class="questionnaire-heading text-center">
                             Questionnaire
@@ -140,7 +140,10 @@
                         </form>-->
                     </div>
                     <div class="customized-toolkit" v-if="step === 5">
-                        <div class="accordion" id="resourcesaccordion">
+                        <div class="go-back" v-if="Object.values(this.question1).every((v) => v === false)">
+                            <p>Please go back to the first page and select the items you are interested in.</p>
+                        </div>
+                        <div v-else class="accordion" id="resourcesaccordion">
                             <div class="accordion-item" v-if="question1.cspslearning">
                                 <h3 class="accordion-header" id="headingOne">
                                     <button class="accordion-button" type="button" data-bs-toggle="collapse"
@@ -322,11 +325,7 @@
         },
         methods: {
             navigateBack: function() {
-                if (this.step === 5) {
-                    this.step = 2
-                } else {
                     this.step--
-                }
             }
         }
     }
@@ -346,8 +345,18 @@
         background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='637' height='637' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23DA797A' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23DA797A'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E");
     }
 
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1rem;
+    }
+
     h1 {
         color: #FFF;
+        background-color: $school-purple;
+        margin: 0rem;
+        padding: 1rem;
     }
 
     .holster {
@@ -468,6 +477,12 @@
         &.skip {
             background-color: #EEEEEE;
             color: #3F2A56;
+
+            &:hover, &:focus {
+                background-color: $school-purple;
+                color: #fff;
+                outline: 1px solid #fff;
+            }
         }
     }
 
