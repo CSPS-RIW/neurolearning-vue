612713ac2e9c3782790e4934c773b13758553450
rtorres <rene.torres@csps-efpc.gc.ca>
2023-02-23 15:33:27 -0500

    Add router link

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div v-cloak>
-        
-        
-          <router-view :key="$route.path" />
-        
+
+
+    <router-view :key="$route.path" />
+    <router-link to="/xd">xd</router-link>
+
   </div>
 </template>
