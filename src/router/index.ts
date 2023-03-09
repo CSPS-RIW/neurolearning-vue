c42e5cd60365ca5ea412f2899cb7f3446e5f56c3
noamstul <stul0002@algonquinlive.com>
2023-03-09 10:39:17 -0500

    deleted extra vue files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Path from '../views/Path.vue';
-import Xd from '../views/Xd.vue';
-import Branch from '../views/BranchingScenario.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/path',
-      name: 'Path',
-      component: Path
-    },
-    {
-      path: '/xd',
-      name: 'Xd',
-      component: Xd
-    },
-    {
-      path: '/branch',
-      name: 'Branch',
-      component: Branch
-    }
+    
   ]
 })
 
