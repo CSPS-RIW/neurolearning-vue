9e7dd83ab1e1c1bec80722c50545f36e41f2d95c
noamstul <stul0002@algonquinlive.com>
2023-03-09 10:44:54 -0500

    deleting more extra stuff

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'animate.css'
 import 'jquery'
-import './assets/fontawesome-free-5.9.0-web/css/all.min.css'
 import { createI18n } from 'vue-i18n'
 import messages from '@intlify/unplugin-vue-i18n/messages';
 import './scss/index.scss'
-
-
-import './scss/chat-buble.scss'
 
 const i18n = createI18n({
     legacy: true,
