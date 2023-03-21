a180f3a8ba6ace28b5d77dd26c0a7cfabbe5d363
rtorres <rene.torres@csps-efpc.gc.ca>
2023-03-21 15:41:02 -0400

    import scorm stuff from main ts

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,21 @@ import App from './App.vue'
 import messages from '@intlify/unplugin-vue-i18n/messages'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import './scorm/APIWrapper'
+import './scorm/scorm'
 import 'jquery'
 import { createI18n } from 'vue-i18n'
 import './scss/index.scss'
 import './assets/fontawesome-free-5.9.0-web/css/all.min.css'
 
 const i18n = createI18n({
-    legacy: true,
-    globalInjection: true,
-    locale: "en",
-    fallbackLocale: "en",
-    availableLocales: ["en", "fr"],
-    messages,
-  })
+  legacy: true,
+  globalInjection: true,
+  locale: "en",
+  fallbackLocale: "en",
+  availableLocales: ["en", "fr"],
+  messages,
+})
 
 const app = createApp(App)
 
